1427443a20dd57a197ac81c871a53904c7bf9dfc
tohsig <eric@jotdashed.com>
2015-04-28 10:43:44 -0400

    Experiments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,53 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def get_workspaces(api_key)
-    path = RestClient::Resource.new('https://app.asana.com/api/1.0/workspaces', user: api_key, password: '')
-    workspaces = path.get
-    JSON.parse(workspaces)
+    resource = RestClient::Resource.new('https://app.asana.com/api/1.0/workspaces',
+                                    user: api_key, password: '')
+    workspaces = resource.get
+    JSON.parse(workspaces)['data']
   end
 
   def get_projects(api_key, workspace_id)
-    path = RestClient::Resource.new("https://app.asana.com/api/1.0/workspaces/#{workspace_id}/projects", user: api_key, password: '')
-    projects = path.get
-    JSON.parse(projects)
+    resource = RestClient::Resource.new("https://app.asana.com/api/1.0/workspaces/#{workspace_id}/projects",
+                                    user: api_key, password: '')
+    projects = resource.get
+    JSON.parse(projects)['data']
   end
 
-  def get_tags
+  def get_tasks(api_key, project_id)
+    resource = RestClient::Resource.new("https://app.asana.com/api/1.0/projects/#{project_id}/tasks",
+                                user: api_key, password: '')
+    tasks = resource['?opt_fields=name,assignee,completed'].get
+    JSON.parse(tasks)['data']
+  end
+
+  def points_tally(api_key, project_id)
+    tasks = get_tasks(api_key, project_id)
+    users = collect_users(api_key, tasks)
+    return users
+  end
+
+  def collect_users(api_key, tasks)
+    users = []
+    tasks.each do |task|
+      if task['completed'] == true
+        users << task['assignee']
+      end
+    end
+    users.uniq!
+    users.each do |user|
+      if user != nil
+        user[:name] = find_user_name(api_key, user['id'])
+      end
+    end
+    users
+  end
+
+  def find_user_name(api_key, user_id)
+    resource = RestClient::Resource.new("https://app.asana.com/api/1.0/users/#{user_id}",
+                                    user: api_key, password: '')
+    user = resource.get
+    user = JSON.parse(user)['data']
+    user['name']
   end
 end
--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -1,10 +1,10 @@
 class WorkspacesController < ApplicationController
-
   def index
     @api_key = params[:api_key]
     @workspaces = get_workspaces(@api_key)
-    @workspaces = @workspaces['data']
 
-    @projects = get_projects(@api_key, 27301307915813)
+    @projects   = get_projects(@api_key, 27301307915813)
+    @tasks      = get_tasks(@api_key, 27329260245608)
+    @users      = points_tally(@api_key, 27329260245608)
   end
 end
